65ab86b53b186e1670279a3cff973d9af8f9f404
Leandro Alves <leandro.alves351@gmail.com>
2018-08-07 08:40:26 -0300

    Variáveis em PHP

---
+++ variaveis.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+	<meta http-equiv="Content-Language" content="pt-br">
+	<title>Variáveis PHP</title>
+</head>
+
+<body>
+
+<?php
+
+	$idade = (int)19;
+	$sal = 1825.54;
+
+	$nome = "Leandro";
+
+	//echo $nome." tem ".$idade." anos"; //concatenação
+
+	echo "$nome tem $idade anos"; //Diretamente
+
+/*
+Inteiro (int),(interger)
+Real (real),(float),(double)
+Caractere (string)
+Lógico (???) True=1 False= vazio
+*/
+?>
+
+
+</body>
+
+</html>
+
+
+
+
