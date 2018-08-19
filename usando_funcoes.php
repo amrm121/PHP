b0cbf834993fa7b4025544ecf147da85de1f650f
Leandro Alves <leandro.alves351@gmail.com>
2018-08-19 02:10:16 -0300

    Usando Array

--- usando_funcoes.php
+++ usando_funcoes.php
@@ -1,11 +1,29 @@
 <?php
 
-//Utilização do number_format | Formatação da saida de números
+/*Utilização do number_format | Formatação da saida de números
 
-$x = '100000.00'; 
+    $x = '100000.00'; 
 
-//($x,casas_decimais,'marcação_decimal','marcação_milhar')
+($x,casas_decimais,'marcação_decimal','marcação_milhar')
 
-$y = number_format($x,2,',','.').' Mil'; 
+    $y = number_format($x,2,',','.').' Mil'; 
 
-    echo $y;
+        echo $y;
+
+/*Usando função foreach -> pecorre array
+
+    Gera apenas os valores do array, ex: foreach ( $array as $valor ) { ação }
+    
+    Gera as chaves e seus valores, ex: foreach ( $array as $chave => $valor ) { ação }
+
+/*Usanod array
+
+    $array= array('Maça','Laranja','Ovos');
+
+    foreach ($array as $key => $value){
+
+        echo 'Indíce: '.$key.' Eu gosto de comer '.$array[$key].' no café da manhã. <br/>';
+        
+    }
+
+*/
