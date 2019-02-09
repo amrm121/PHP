bb3d12cc09116629d7b281ba12904fa66b1706d4
Leandro Alves <leandro.alves351@gmail.com>
2019-02-09 16:20:14 -0300

    operadores bitwise

---
+++ 2_10_Operadores_bitwise.php
@@ -0,0 +1,14 @@
+<?php echo '<center>';
+
+//Movendo os bits para direita
+print(8<<9); //Equação -> Esquerda * 2 ^ Direita
+
+echo '<br>';
+
+//Movendo os bits para esquerda
+print(8>>9); //Equação -> Esquerda / 2 ^ Direita
+
+
+echo '<br>';
+
+echo __NAMESPACE__;
