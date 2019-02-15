e98fd37d9f3a3d2ba07b52a3e7eacf4807a0a51a
Leandro Alves <leandro.alves351@gmail.com>
2019-02-14 23:43:17 -0300

    Strings padroes

---
+++ 3_Strings_padroes.php
@@ -0,0 +1,24 @@
+<?php
+/* NOWDOC
+    $str= <<<'A'
+    Usando nowdoc.
+    A;
+
+
+/* HEREDOC
+    $str= <<<"A"
+    Usando nowdoc.
+    A;
+
+
+*/
+
+
+
+
+//strstr
+
+
+$e = 'leandro@gmail.com';
+
+print strstr($e, 'd');
