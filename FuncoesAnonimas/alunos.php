b1c5b09162520b3a29954cdde1c93b39a07e55a1
Leandro Alves Magalhães dos Santos <Leandro.alves351@gmail.com>
2019-03-06 16:26:51 -0300

    Valor da idade modificados para inteiro

--- FuncoesAnonimas/alunos.php
+++ FuncoesAnonimas/alunos.php
@@ -7,34 +7,34 @@ $alunos  = [
     ],
     [
         'nome'  => 'Leticia Alves',
-        'idade' => '15'   
+        'idade' => 15  
     ],
     [
         'nome'  => 'Paula Rebeca',
-        'idade' => '29'    
+        'idade' => 29    
     ],
     [
         'nome'  => 'Julia Santos',
-        'idade' => '65'     
+        'idade' => 65     
     ],
     [
         'nome'  => 'Lúcia Magalhães Santos',
-        'idade' => '45'     
+        'idade' => 45     
     ],
     [
         'nome'  => 'Augusto José',
-        'idade' => '55'     
+        'idade' => 55     
     ],
     [
         'nome'  => 'Raimundo Mário',
-        'idade' => '51'     
+        'idade' => 51     
     ],
     [
         'nome'  => 'Antônio Ferreira',
-        'idade' => '70'    
+        'idade' => 70    
     ],
     [
         'nome'  => 'Francisco Pedreiro',
-        'idade' => '53'    
+        'idade' => 53    
     ],
     ];
