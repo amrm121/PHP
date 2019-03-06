183e26539adfa9a7d1a346a99aeb2560a437b97d
Leandro Alves Magalhães dos Santos <Leandro.alves351@gmail.com>
2019-03-06 15:42:34 -0300

    Cria funções de filtro. Passa como segundo parâmetro no array_filter

--- FuncoesAnonimas/funcoes_anonimas.php
+++ FuncoesAnonimas/funcoes_anonimas.php
@@ -3,10 +3,12 @@
 require ('notas.php');
 require ('alunos.php');
 
-//Função anonima do PHP
-$res = array_filter($notas, function($nota){
+//Cria filtro para notas
+$filtro = function($nota){
+    return $nota < 50;
+};
 
-    return $nota > 50;
-});
+//Segundo parâmetro chama filtro
+$res = array_filter($notas, $filtro);
 
 var_dump($res);
