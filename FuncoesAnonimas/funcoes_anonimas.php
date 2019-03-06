8245e948f35aa6b5db7c64638e2321c31e355735
Leandro Alves Magalhães dos Santos <Leandro.alves351@gmail.com>
2019-03-06 16:30:48 -0300

    Função anônima

--- FuncoesAnonimas/funcoes_anonimas.php
+++ FuncoesAnonimas/funcoes_anonimas.php
@@ -4,15 +4,15 @@ require ('notas.php');
 require ('alunos.php');
 
 //Filtro de idade
-function maior($elem){
+$idade = function($elem){
     return $elem['idade'] > 50;
-}
+};
 
 //Filtro média
-function reprovado($elem){
+$rep = function($elem){
 
     return $elem < 50;
-}
+};
 
 
 /*Criando própria função callback
@@ -36,4 +36,4 @@ function filtroArray(Array $array, $nomeFuncao){
     return $filtro;
 }
 
-var_dump(filtroArray($alunos, 'maior'));
+var_dump(filtroArray($notas, $rep));
