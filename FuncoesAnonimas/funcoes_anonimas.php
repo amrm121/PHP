96c15088585f9ec2a4821aa0c500f708217618e3
Leandro Alves Magalhães dos Santos <Leandro.alves351@gmail.com>
2019-03-06 15:44:19 -0300

    Chama função como string

--- FuncoesAnonimas/funcoes_anonimas.php
+++ FuncoesAnonimas/funcoes_anonimas.php
@@ -4,11 +4,11 @@ require ('notas.php');
 require ('alunos.php');
 
 //Cria filtro para notas
-$filtro = function($nota){
-    return $nota < 50;
+function filtro($nota){
+    return $nota > 50;
 };
 
-//Segundo parâmetro chama filtro
-$res = array_filter($notas, $filtro);
+//Segundo parâmetro chama função filtro como string
+$res = array_filter($notas, 'filtro');
 
 var_dump($res);
