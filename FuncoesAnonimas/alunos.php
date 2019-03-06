e0750951d19065265e91001c10d23cbc5fa330cd
Leandro Alves Magalhães dos Santos <Leandro.alves351@gmail.com>
2019-03-06 15:35:23 -0300

    Array de alunos

---
+++ FuncoesAnonimas/alunos.php
@@ -0,0 +1,40 @@
+<?php namespace FuncoesAnonimas\Alunos;
+
+$alunos  = [
+    [
+        'nome'  => 'Leandro Alves',
+        'idade' => 20    
+    ],
+    [
+        'nome'  => 'Leticia Alves',
+        'idade' => '15'   
+    ],
+    [
+        'nome'  => 'Paula Rebeca',
+        'idade' => '29'    
+    ],
+    [
+        'nome'  => 'Julia Santos',
+        'idade' => '65'     
+    ],
+    [
+        'nome'  => 'Lúcia Magalhães Santos',
+        'idade' => '45'     
+    ],
+    [
+        'nome'  => 'Augusto José',
+        'idade' => '55'     
+    ],
+    [
+        'nome'  => 'Raimundo Mário',
+        'idade' => '51'     
+    ],
+    [
+        'nome'  => 'Antônio Ferreira',
+        'idade' => '70'    
+    ],
+    [
+        'nome'  => 'Francisco Pedreiro',
+        'idade' => '53'    
+    ],
+    ];
