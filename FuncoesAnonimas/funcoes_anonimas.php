d8a1f57cd8c796d119588cf6b5826324dbf2ba64
Leandro Alves Magalhães dos Santos <Leandro.alves351@gmail.com>
2019-03-06 16:27:17 -0300

    Crianção de filtro para função arry_filter

--- FuncoesAnonimas/funcoes_anonimas.php
+++ FuncoesAnonimas/funcoes_anonimas.php
@@ -3,15 +3,37 @@
 require ('notas.php');
 require ('alunos.php');
 
-//Filtro de array
+//Filtro de idade
+function maior($elem){
+    return $elem['idade'] > 50;
+}
 
-$notasFiltro = [];
+//Filtro média
+function reprovado($elem){
 
-foreach($notas as $nota):
-    if($nota > 50){
+    return $elem < 50;
+}
 
-        $notasFiltro[] = $nota;
-    }
-endforeach;
 
-var_dump($notasFiltro);
+/*Criando própria função callback
+
+Recebe array de notas ou alunos
+Recebe como segundo parâmetro uma função, como lógica especifica da função
+
+
+*/
+function filtroArray(Array $array, $nomeFuncao){
+
+    $filtro = [];
+
+    foreach($array as $elem):
+        if($nomeFuncao($elem)){
+
+            $filtro[] = $elem;
+        }
+    endforeach;
+
+    return $filtro;
+}
+
+var_dump(filtroArray($alunos, 'maior'));
